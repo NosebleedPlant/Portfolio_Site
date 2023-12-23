e4a7d876356b3898fefa0a146f196132ce2d40e1
NosebleedPlant <yalmazabdullah.main@gmail.com>
2023-12-23 08:10:39 -0700

    working markdown routing

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </>
+  );
+}
